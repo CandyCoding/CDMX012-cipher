7b2670b5780cb39b0f4c8adc9f28bbd124d7375e
Candy Lopez <candy.lopez.gr@gmail.com>
2022-01-25 12:04:11 -0600

    Versión con test aprobados pero sin estilizado con CSS

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,48 @@
 const cipher = {
-  // ...
+encode : (offset,texto) =>{
+    if(offset === 0 || texto==="") // limitamos el funcionamiento de nuestra función si se busca invocar con un argumento erroneo.
+    {throw new TypeError("Ingresa un texto y desplazamiento para cifrar");}
+    else {if (offset === null|| texto === 0)
+        {throw new TypeError("Ingresa un texto y desplazamiento para cifrar");}
+    else {
+    let codedMsj= ""; // string vacía que contendra los resultados de nuestra fórmula. 
+    for (let i = 0; i < texto.length; i++) { // el ciclo inicia revisando todas las letras 
+            let plainLetters = texto.charAt(i);
+        //sin esto sólo lee la primera letra del string y le aplica la formula a todo el string pero con el primer valor.
+            let letraAscii = texto.charCodeAt(i);
+        //cambiando de alfabeto latino a Ascii
+                if ((letraAscii >= 65) && (letraAscii<=90)) { // parametro para que realice la formula con mayúsculas .
+                 plainLetters= String.fromCharCode((letraAscii - 65 + parseInt(offset))%26+65);
+                } // obtenemos el string en alfabeto latino derivado del resultado de la fórmula Mich
+         else if ((letraAscii >= 97) && (letraAscii<=122)){ //parametro para que realice la fórmula con minúsculas.
+                plainLetters= String.fromCharCode((letraAscii - 97 + parseInt(offset))%26+97);
+         }
+    codedMsj += plainLetters; //hacemos una concatenación en el string vacía 
+    }
+    return codedMsj; //cortamos el proceso del ciclo 
+    // ya por fin funcionó ya no le muevas porfi !!!! 
+        }
+    }
+},
+    //Ahora comenzamos con la función de decodificar 
+    decode : (offset,texto) => {
+        if(offset === 0 || texto==="") {throw new TypeError("Ingresa un texto y desplazamiento para descifrar");}
+        else{if (offset=== null|| texto=== 0){throw new TypeError("Ingresa un texto y desplazamento para descifrar")}
+        let toDecodeMsj = "";
+        for (let i = 0; i < texto.length; i ++) {
+                let codedLetters = texto.charAt(i);
+                let letraAscii = texto.charCodeAt(i);
+                    if ((letraAscii >= 65 ) && (letraAscii <= 90)){
+                    codedLetters = String.fromCharCode((letraAscii- 90 - parseInt(offset))%26+90);
+                    }
+                    else if ((letraAscii >= 97) && (letraAscii <= 122)) {
+                    codedLetters = String.fromCharCode((letraAscii - 122 - parseInt(offset))%26+122);
+                    }
+            toDecodeMsj += codedLetters;
+        }
+        return toDecodeMsj;
+    }
+// ya tampoco le muevas a esto!!! 
+    }
 };
-
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import cipher from './cipher.js';
+// variables de botón, texto y  offset que ingrese el usuario, además del campo que imprimirá los resultados en pantalla 
+let btnCifrar = document.getElementById("encodeBtn");
+let btnDescifrar = document.getElementById("decodeBnt");
+let texto = document.getElementById("plainText");
+let offset = document.getElementById("offset");
+let mensajeFinal = document.getElementById("tuMensajeCodificado");
 
-console.log(cipher);
+// Evento para el botón codificar. 
+btnCifrar.addEventListener("click",(event) =>{
+    event.preventDefault();
+    mensajeFinal.innerHTML= cipher.encode(offset.value,texto.value); 
+});
+//Evento para el botón decodificar.
+btnDescifrar = addEventListener("dblclick",(event) =>{
+    event.preventDefault(btnDescifrar);
+    mensajeFinal.innerHTML= cipher.decode(offset.value,texto.value); 
+});
